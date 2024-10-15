d1692090935b1665a8f0ff1df991e35de7ab93f6
ibrahim-innospace <ibrahim@innospace.tech>
2024-10-15 10:50:43 +0600

    Minor changes

--- lib/features/in_app_purchase/bloc/purchase_bloc.dart
+++ lib/features/in_app_purchase/bloc/purchase_bloc.dart
@@ -9,33 +9,6 @@ import '../services/purchase_service.dart';
 import 'purchase_event.dart';
 import 'purchase_state.dart';
 
-// class PurchaseBloc extends Bloc<PurchaseEvent, PurchaseState> {
-//   //final PurchaseService _purchaseService;
-
-//   final ApiServiceInterface _purchaseService;
-
-//   PurchaseBloc(this._purchaseService) : super(PurchaseInitial()) {
-//     on<PurchaseCourse>(_onPurchaseCourse);
-//   }
-
-//   Future<void> _onPurchaseCourse(
-//       PurchaseCourse event, Emitter<PurchaseState> emit) async {
-//     emit(PurchaseLoading());
-//     try {
-//       final success = await _purchaseService.purchaseCourse(event.courseId);
-//       if (success) {
-//         mockCourses
-//             .firstWhere((course) => course.id == event.courseId)
-//             .isPurchased = true;
-//         emit(PurchaseSuccess());
-//       } else {
-//         emit(PurchaseError('Purchase failed'));
-//       }
-//     } catch (e) {
-//       emit(PurchaseError(e.toString()));
-//     }
-//   }
-// }
 class PurchaseBloc extends Bloc<PurchaseEvent, PurchaseState> {
   final PurchaseRepository _repository;
 
